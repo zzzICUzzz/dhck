22c1116b8d5f40cee2626787201a3ac4ccebf0ed
Z <147078393+zzzICUzzz@users.noreply.github.com>
2024-06-05 19:03:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Kiểm tra nếu người dùng đã truy cập index.html trong phiên làm việc hiện tại
+if (!sessionStorage.getItem('visitedIndex')) {
+    // Nếu chưa, kiểm tra nếu trang hiện tại không phải là index.html
+    if (!window.location.pathname.endsWith('index.html') && !window.location.pathname.endsWith('/')) {
+        // Chuyển hướng tới index.html
+        window.location.href = '/index.html';
+    } else {
+        // Nếu là index.html, lưu trạng thái đã truy cập trong phiên làm việc hiện tại
+        sessionStorage.setItem('visitedIndex', 'true');
+    }
+}
     const files = {
         "gp1_220.json": 10,
         "ngoai966_1438.json": 10,
